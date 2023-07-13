6c9c18fe9201b707220622af255e23aff9f87aa0
NabilAzizii <nabilazizi109@gmail.com>
2023-07-13 22:40:36 +0700

    LandingPage

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Typed from 'react-typed';
+import Typewriter from 'typewriter-effect';
 
 const Hero = () => {
   return (
@@ -8,7 +8,15 @@ const Hero = () => {
         <p className="text-[#00df9a] font-bold p-2">GROWING WITH DATA ANALYTICS</p>
         <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">Transforming Data into Opportunities</h1>
         <div className="flex justify-center items-center">
-          <Typed className="md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2 py-4" strings={['Data Unleashed.', 'Success Achieved.', 'Partner with Our Analytics.']} typeSpeed={120} backSpeed={140} loop />
+          <div className="md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2 py-4">
+            <Typewriter
+              options={{
+                strings: ['Data Unleashed.', 'Success Achieved.', 'Partner with Our Analytics.'],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </div>
         </div>
         <p className="md:text-2xl text-xl font-bold text-gray-500">Unlocking Insights, Empowering Growth: Your Data's Greatest Ally</p>
         <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black">Get Started</button>
